b6b3287f0fd4620f57d71a89e51f0ee261cfab87
TC211736 <TC211736@reigate.ac.uk>
2022-10-03 12:22:34 +0100

    Initial commit

--- core/src/com/mygdx/game/LoginScreen.java
+++ core/src/com/mygdx/game/LoginScreen.java
@@ -1,8 +1,11 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
@@ -16,44 +19,61 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 public class LoginScreen extends ScreenAdapter {
     MyGdxGame game;
     protected Stage stage;
-    private TextureAtlas atlas;
-    Camera camera;
     SpriteBatch batch;
-    Table mainframe;
     Skin skin;
 
     public LoginScreen(MyGdxGame game) {
         this.game = game;
-    }
-
-    public LoginScreen() {
-        atlas = new TextureAtlas("uiskin.atlas");
-        skin = new Skin();
-        skin.addRegions(atlas);
 
         batch = new SpriteBatch();
+        //atlas = new TextureAtlas("uiskin.atlas");
+        skin = new Skin(Gdx.files.internal("uiskin.json"));
+        //skin.addRegions(atlas);
 
-        camera = new OrthographicCamera();
-        stage = new Stage(new FitViewport(500, 500, camera),batch);
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+        TextButton playButton = new TextButton("Play", skin);
+        playButton.setPosition(300, 300);
+        playButton.setSize(300, 60);
+
+        stage.addActor(playButton);
+
+        /*camera = new OrthographicCamera();
+
 
         camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
         camera.update();
 
+        */
+
     }
+
 
     @Override
     public void show() {
-        game.setScreen(new TitleScreen(game));
-        skin = new Skin();
-        Gdx.input.setInputProcessor(stage);
 
-        mainframe = new Table();
+        Gdx.input.setInputProcessor(new InputAdapter() {
+            @Override
+            public boolean keyDown(int keyCode) {
+                if (keyCode == Input.Keys.SPACE) {
+                    game.setScreen(new TitleScreen(game));
+                }
+                return true;
+            }
+        });
+
+        Gdx.gl.glClearColor(0, 0.25f, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+
+        /*mainframe = new Table();
         mainframe.setFillParent(true);
 
         mainframe.top();
 
-        TextButton playbutton = new TextButton("Play", skin);
+
         TextButton optionsButton = new TextButton("Options", skin);
-        TextButton exitButton = new TextButton("Exit", skin);
+        TextButton exitButton = new TextButton("Exit", skin); */
     }
 }
